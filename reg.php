cab78c436274d8b6526fafbea91c7576ebb48ad3
jquintinita <joseph.quintinita@gmail.com>
2024-02-27 16:08:00 +0800

    updated

---
+++ reg.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbcricket";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+
+$fname = $_POST["fullname"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+
+
+$strSql = "INSERT INTO registerusers (fullName, phone, email) VALUES ('$fname', '$phone', '$email')";
+
+if(mysqli_query($conn, $strSql)){
+    echo "Submission is successfully recorded";
+}else{
+    echo mysqli_error();
+}
+
+?>
